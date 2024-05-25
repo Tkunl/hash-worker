378e5d0c79e500253c3c41ce62958f8fbcfc7f34
Tkunl <silence000@Foxmail.com>
2024-05-25 19:41:45 +0800

    feat: 重新配了一下 rollup.config.ts

--- packages/core/rollup.config.ts
+++ packages/core/rollup.config.ts
@@ -12,20 +12,29 @@ const bundleName = 'kunHash'
 // 关于 iife/umd 面向现代的前端提供 iife 就可以了。
 // 因此你不需要过多复杂的配置。
 
+function workerPluginsConfig() {
+  return webWorkerLoader({
+    extensions: ['.ts'],
+  })
+}
+
 export default defineConfig([
+  // esm 格式产物
   {
     input: 'src/main.ts',
     output: [
       { file: 'dist/index.mjs', format: 'esm', exports: 'named' },
       { file: 'dist/index.js', format: 'cjs', exports: 'named' },
     ],
-    plugins: [nodeResolve(), swc(), webWorkerLoader({ extensions: ['.ts'] })],
+    plugins: [nodeResolve(), swc(), workerPluginsConfig()],
   },
+  // esm 类型产物
   {
     input: 'src/main.ts',
     output: { file: 'dist/index.d.ts' },
     plugins: [dts()],
   },
+  // iife 格式产物
   {
     input: 'src/main.ts',
     output: { file: 'dist/global.js', format: 'iife', name: bundleName },
@@ -33,8 +42,10 @@ export default defineConfig([
       nodeResolve(),
       swc(),
       minify({ mangle: true, module: true, compress: true, sourceMap: true }),
+      workerPluginsConfig(),
     ],
   },
+  // iife 类型产物
   {
     input: 'src/iife.ts',
     output: { file: 'dist/global.d.ts', format: 'es' },
