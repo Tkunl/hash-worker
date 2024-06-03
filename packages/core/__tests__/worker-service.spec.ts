1f203ec0ce2d72f010955729c30c483f1c3ec90e
Tkunl <silence000@Foxmail.com>
2024-06-03 22:08:58 +0800

    feat: added test case

--- packages/core/__tests__/worker-service.spec.ts
+++ packages/core/__tests__/worker-service.spec.ts
@@ -1,26 +1,26 @@
-import test from 'ava';
-import sinon from 'sinon';
+import test from 'ava'
+import sinon from 'sinon'
 import proxyquire from 'proxyquire'
 import { WorkerPoolForMd5s } from '../src/worker/worker-pool-for-md5s'
 import { WorkerPoolForCrc32s } from '../src/worker/worker-pool-for-crc32s'
 
 function createMockArrayBuffer(size: number): ArrayBuffer {
-  return new Uint8Array(size).buffer;
+  return new Uint8Array(size).buffer
 }
 
 // 模拟 WorkerPoolForMd5s 和 WorkerPoolForCrc32s
 class MockWorkerPoolForMd5s extends WorkerPoolForMd5s {
-  exec = sinon.stub().resolves(['md5-result']);
+  exec = sinon.stub().resolves(['md5-result'])
 }
 
 class MockWorkerPoolForCrc32s extends WorkerPoolForCrc32s {
-  exec = sinon.stub().resolves(['crc32-result']);
+  exec = sinon.stub().resolves(['crc32-result'])
 }
 
 const MockWorker = class {
-  postMessage = sinon.stub();
-  terminate = sinon.stub();
-};
+  postMessage = sinon.stub()
+  terminate = sinon.stub()
+}
 
 // TODO 此处依赖替换有问题
 const { WorkerService } = proxyquire('../src/worker/worker-service', {
@@ -30,17 +30,16 @@ const { WorkerService } = proxyquire('../src/worker/worker-service', {
   '../src/worker/worker-pool-for-crc32s': proxyquire('./../src/worker/crc32-single.web-worker.ts', {
     'web-worker:./crc32-single.web-worker.ts': MockWorker,
   }),
-}).noCallThru();
+}).noCallThru()
 
+test('WorkerService should compute MD5 for files', async (t) => {
+  const mockMd5Pool = new MockWorkerPoolForMd5s(2)
+  const workerService = new WorkerService(2)
+  workerService.md5SingleWorkerPool = mockMd5Pool
 
-test('WorkerService should compute MD5 for files', async t => {
-  const mockMd5Pool = new MockWorkerPoolForMd5s(2);
-  const workerService = new WorkerService(2);
-  workerService.md5SingleWorkerPool = mockMd5Pool;
+  const chunks = [createMockArrayBuffer(1024), createMockArrayBuffer(2048)]
+  const result = await workerService.getMD5ForFiles(chunks)
 
-  const chunks = [createMockArrayBuffer(1024), createMockArrayBuffer(2048)];
-  const result = await workerService.getMD5ForFiles(chunks);
-
-  t.deepEqual(result, ['md5-result']);
-  t.true(mockMd5Pool.exec.calledOnceWithExactly(chunks));
-});
+  t.deepEqual(result, ['md5-result'])
+  t.true(mockMd5Pool.exec.calledOnceWithExactly(chunks))
+})
