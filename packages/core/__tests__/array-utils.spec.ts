e399cdcd531c3fbe6a5c149994713431929dc7a3
Tkunl <silence000@foxmail.com>
2024-06-04 21:51:15 +0800

    feat: update formate

--- packages/core/__tests__/array-utils.spec.ts
+++ packages/core/__tests__/array-utils.spec.ts
@@ -1,64 +1,52 @@
-import test from 'ava';
+import test from 'ava'
 import { getArrParts } from '../src/utils'
 
-test('getArrParts should split array into parts of given size', t => {
-  const input = [1, 2, 3, 4, 5, 6, 7];
-  const size = 3;
-  const expectedOutput = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7]
-  ];
+test('getArrParts should split array into parts of given size', (t) => {
+  const input = [1, 2, 3, 4, 5, 6, 7]
+  const size = 3
+  const expectedOutput = [[1, 2, 3], [4, 5, 6], [7]]
 
-  const result = getArrParts<number>(input, size);
+  const result = getArrParts<number>(input, size)
 
-  t.deepEqual(result, expectedOutput);
-});
+  t.deepEqual(result, expectedOutput)
+})
 
-test('getArrParts should handle empty array', t => {
-  const input: number[] = [];
-  const size = 3;
-  const expectedOutput: number[][] = [];
+test('getArrParts should handle empty array', (t) => {
+  const input: number[] = []
+  const size = 3
+  const expectedOutput: number[][] = []
 
-  const result = getArrParts<number>(input, size);
+  const result = getArrParts<number>(input, size)
 
-  t.deepEqual(result, expectedOutput);
-});
+  t.deepEqual(result, expectedOutput)
+})
 
-test('getArrParts should handle size larger than array length', t => {
-  const input = [1, 2, 3];
-  const size = 5;
-  const expectedOutput = [
-    [1, 2, 3]
-  ];
+test('getArrParts should handle size larger than array length', (t) => {
+  const input = [1, 2, 3]
+  const size = 5
+  const expectedOutput = [[1, 2, 3]]
 
-  const result = getArrParts<number>(input, size);
+  const result = getArrParts<number>(input, size)
 
-  t.deepEqual(result, expectedOutput);
-});
+  t.deepEqual(result, expectedOutput)
+})
 
-test('getArrParts should handle size of 1', t => {
-  const input = [1, 2, 3];
-  const size = 1;
-  const expectedOutput = [
-    [1],
-    [2],
-    [3]
-  ];
+test('getArrParts should handle size of 1', (t) => {
+  const input = [1, 2, 3]
+  const size = 1
+  const expectedOutput = [[1], [2], [3]]
 
-  const result = getArrParts<number>(input, size);
+  const result = getArrParts<number>(input, size)
 
-  t.deepEqual(result, expectedOutput);
-});
+  t.deepEqual(result, expectedOutput)
+})
 
-test('getArrParts should handle size equal to array length', t => {
-  const input = [1, 2, 3];
-  const size = 3;
-  const expectedOutput = [
-    [1, 2, 3]
-  ];
+test('getArrParts should handle size equal to array length', (t) => {
+  const input = [1, 2, 3]
+  const size = 3
+  const expectedOutput = [[1, 2, 3]]
 
-  const result = getArrParts<number>(input, size);
+  const result = getArrParts<number>(input, size)
 
-  t.deepEqual(result, expectedOutput);
-});
+  t.deepEqual(result, expectedOutput)
+})
--- packages/core/__tests__/worker-pool.spec.ts
+++ packages/core/__tests__/worker-pool.spec.ts
@@ -8,55 +8,56 @@ import sinon from 'sinon'
 class MockWorkerWrapper extends WorkerWrapper {
   constructor() {
     super({
-      terminate: () => {}
-    } as Worker);
-    this.status = StatusEnum.WAITING;
+      terminate: () => {},
+    } as Worker)
+    this.status = StatusEnum.WAITING
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   run<T>(param: ArrayBuffer, params: ArrayBuffer[], index: number) {
-    return Promise.resolve('result' as unknown as T);
+    return Promise.resolve('result' as unknown as T)
   }
 }
 
 // 模拟 MiniSubject
 class MockMiniSubject<T> extends MiniSubject<T> {
   constructor(value: T) {
-    super(value);
+    super(value)
   }
 
   next(value: T) {
-    this._value = value;
-    this.subscribers.forEach((cb) => cb(value));
+    this._value = value
+    this.subscribers.forEach((cb) => cb(value))
   }
 }
 
 // 扩展 WorkerPool 以使用模拟的 WorkerWrapper 和 MiniSubject
 class TestWorkerPool extends WorkerPool {
   constructor(maxWorkers = 4) {
-    super(maxWorkers);
-    this.curRunningCount = new MockMiniSubject(0);
+    super(maxWorkers)
+    this.curRunningCount = new MockMiniSubject(0)
     for (let i = 0; i < maxWorkers; i++) {
-      this.pool.push(new MockWorkerWrapper());
+      this.pool.push(new MockWorkerWrapper())
     }
   }
 }
 
-test('WorkerPool should execute tasks and return results', async t => {
-  const workerPool = new TestWorkerPool(2);
-  const params = [new ArrayBuffer(8), new ArrayBuffer(8)];
+test('WorkerPool should execute tasks and return results', async (t) => {
+  const workerPool = new TestWorkerPool(2)
+  const params = [new ArrayBuffer(8), new ArrayBuffer(8)]
 
-  const results = await workerPool.exec<string>(params);
+  const results = await workerPool.exec<string>(params)
 
-  t.deepEqual(results, ['result', 'result']);
-});
+  t.deepEqual(results, ['result', 'result'])
+})
 
-test('WorkerPool should terminate all workers', t => {
-  const workerPool = new TestWorkerPool(2);
-  const terminateSpies = workerPool.pool.map(worker => sinon.spy(worker, 'terminate'));
+test('WorkerPool should terminate all workers', (t) => {
+  const workerPool = new TestWorkerPool(2)
+  const terminateSpies = workerPool.pool.map((worker) => sinon.spy(worker, 'terminate'))
 
-  workerPool.terminate();
+  workerPool.terminate()
 
-  terminateSpies.forEach(spy => {
-    t.true(spy.calledOnce);
-  });
-});
+  terminateSpies.forEach((spy) => {
+    t.true(spy.calledOnce)
+  })
+})
