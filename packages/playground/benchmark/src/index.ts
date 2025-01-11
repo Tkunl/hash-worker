9782a03fa8cbd0872bfb9307cd637c251b8ccdc8
Tkunl <tkunl@foxmail.com>
2025-01-11 16:37:21 +0800

    feat: 更新了部分项目依赖

--- packages/playground/benchmark/src/index.ts
+++ packages/playground/benchmark/src/index.ts
@@ -4,13 +4,7 @@ import { Strategy } from 'hash-worker'
 const options: BenchmarkOptions = {
   sizeInMB: 100,
   workerCountTobeTest: [ 8, 8, 8 ],
-  strategy: Strategy.crc32
+  strategy: Strategy.md5
 }
 
-// const toMB = (bytes: number) => (bytes / 1024 / 1024).toFixed(2)
-// const beforeMemoryUsage = process.memoryUsage()
-// console.log('beforeMemoryUsage', toMB(beforeMemoryUsage.rss))
-benchmark(options).then(() => {
-  // const afterMemoryUsage = process.memoryUsage()
-  // console.log('afterMemoryUsage', toMB(afterMemoryUsage.rss))
-})
+benchmark(options).then(() => {})
