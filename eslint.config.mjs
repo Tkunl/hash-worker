15ee9776d762cf07ebcb7e7bf272a1ddfbb178d5
Tkunl <tkunl@foxmail.com>
2025-05-21 18:00:04 +0800

    Revert "feat: 去掉了无用的 eslint 包"
    This reverts commit e247ade20239585e8a286001c4a7ea5f64e1ff11.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,35 +1,53 @@
 import typescriptEslint from '@typescript-eslint/eslint-plugin'
+import prettier from 'eslint-plugin-prettier'
 import globals from 'globals'
 import tsParser from '@typescript-eslint/parser'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
 import js from '@eslint/js'
-import prettierConfig from 'eslint-config-prettier'
+import { FlatCompat } from '@eslint/eslintrc'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+})
 
 export default [
   {
     ignores: ['**/node_modules/', '**/output/', '**/dist/', '**/playground/'],
   },
-  js.configs.recommended,
-  ...typescriptEslint.configs.recommended,
-  prettierConfig,
+  ...compat.extends(
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ),
   {
     plugins: {
       '@typescript-eslint': typescriptEslint,
+      prettier,
     },
+
     languageOptions: {
       globals: {
         ...globals.browser,
         ...globals.node,
       },
+
       parser: tsParser,
       ecmaVersion: 2022,
       sourceType: 'module',
     },
+
     rules: {
       'prettier/prettier': 'error',
       '@typescript-eslint/no-unused-vars': 'error',
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/ban-ts-comment': 'off',
       '@typescript-eslint/no-require-imports': 'off',
+
       '@typescript-eslint/no-unused-expressions': [
         'error',
         {
